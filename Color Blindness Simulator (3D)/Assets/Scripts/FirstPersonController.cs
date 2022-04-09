8435fb85920ac5a8018d447a28bba5ffa9d6aa13
Devon Wilkes <devonowilkes@hotmail.com>
2022-04-09 17:01:42 -0400

    Imported a Terrain Asset, made first person movement script
    Created movement for the player as well as downloading a terrain asset to use later for level details.

---
+++ Color Blindness Simulator (3D)/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FirstPersonController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.8f;
+    Vector3 velocity;
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    bool runPress = false;
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            runPress = !runPress;
+        }
+
+        if (runPress == true)
+        {
+            speed = 25f;
+        }
+        else
+        {
+            speed = 12f;
+        }
+
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
